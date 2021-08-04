e24647faff684d10c1df26f70613a795c0c69994
Bruna Nunes <bruna.f.n@hotmail.com>
2021-08-04 11:13:07 -0300

    salvando dados do evento

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,38 @@
+// essa é a função responsável por salvar a subscription no faunaDB
+// buscar o usuário no banco do fauna com o ID customerId (stripe_customer_id)
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+){
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
+}
